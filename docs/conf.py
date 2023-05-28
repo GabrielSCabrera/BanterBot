48cc60f1584a03b203092ece3e848d7a530865e5
GabrielSCabrera <gabrielscabrera@gmail.com>
2023-05-28 21:44:59 +0200

    Update Docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-# import os
-# import sys
-#
-# sys.path.insert(0, os.path.abspath("../banterbot"))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
 
 project = "BanterBot"
 copyright = "2023, Gabriel S. Cabrera"
