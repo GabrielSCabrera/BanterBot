20885ef5ffa31d0ce6c8c9b5f73d24d4252acd13
GabrielSCabrera <gabrielscabrera@gmail.com>
2023-06-01 00:53:43 +0200

    Remove test files

--- test.py
+++
@@ -1,8 +0,0 @@
-from banterbot import BanterBotTK, get_model_by_name, get_voice_by_name
-
-model = get_model_by_name("gpt-3.5-turbo")
-voice = get_voice_by_name("Aria")
-style = "chat"
-
-app = BanterBotTK(model=model, voice=voice, style=style)
-app.run()
