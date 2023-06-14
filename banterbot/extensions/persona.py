8c6320997ba10dbad11b3cf6717af32dca071055
GabrielSCabrera <gabrielscabrera@gmail.com>
2023-06-15 00:37:36 +0200

    Fix incorrect import, pt 2

--- banterbot/extensions/persona.py
+++ banterbot/extensions/persona.py
@@ -2,7 +2,8 @@ import bisect
 from typing import List
 
 from banterbot.protos import memory_pb2
-from banterbot.utils import Memory, Message
+from banterbot.utils.memory import Memory
+from banterbot.utils.message import Message
 
 
 class Persona:
